8e72d68db96e7d2e097e3502b0c7852badb6f813
Abdurahmon <abdurahmonikromov634@gmail.com>
2024-10-25 20:26:20 +0500

    homework survey

---
+++ JS/main.js
@@ -0,0 +1,22 @@
+var greeting = alert("salom, yaxshimisiz")
+var username = prompt ("bizga umuman farqi yo'q, ismingizni kiriting")
+console.log(username);
+var age = confirm("16 yoshdan kattamisiz?")
+console.log(age);
+var warning = confirm("Oldinda nima chiqishidan qat'iy nazar unga tayyormisiz?")
+console.log(warning);
+var agreement = prompt("Bu qonuniy bo'lishi uchun roziligingizni yozing:")
+console.log(agreement);
+var gameplayRule = alert("Biz sizga inkor qilish mumkin savol, yoki rozi bo'lish shart bo'lgan savollar beramiz.")
+var example = confirm("Masalan, buni inkor qilish mumkin")
+var start = alert("Ketdikmi?")
+var comfort = confirm("O'zingizni qulay his qilyapsizmi?")
+console.log("qulaylik", comfort);
+var whyYouExist = confirm("Dunyoda yashashingizdan sabab nima ekanligini o'ylab korgamisiz?")
+console.log("Overthinker", whyYouExist);
+var reason = prompt("O'sha sabab nima?(o'ylamagan bo'lsangiz ham yozing)")
+console.log("Reason to live", reason);
+var lightOut = confirm("Agar, chiroq o'chsa qo'rqib ketarmidingiz?")
+console.log("Scared", lightOut);
+var fact = alert("sizga, bir fakt aytaymi?")
+var right = confirm("siz najot ta'limda ishlaysiz yoki o'qiysiz!")
